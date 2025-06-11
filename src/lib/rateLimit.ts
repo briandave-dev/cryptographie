9773e288a84cd03dba7a2fdb424b5122cd531f61
unknown <briandave.dev@gmail.com>
2025-06-11 17:37:59 +0100

    removed rate limit ts file

--- src/lib/rateLimit.ts
+++
@@ -1,6 +0,0 @@
-import { Ratelimit } from "@upstash/ratelimit";
-
-export const rateLimit = new Ratelimit({
-  redis: redis,
-  limiter: Ratelimit.slidingWindow(10, "1 m"), // 10 requêtes par minute
-});
